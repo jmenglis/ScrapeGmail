06866df3ac47bf6d69d2192337d49b7263592d3f
jmenglis <josh@joshenglish.com>
2016-06-13 15:27:47 -0500

    Adding the database, models and entity framework to the project

---
+++ ScrapeGmail/MainDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ScrapeGmail.Models;
+
+namespace ScrapeGmail {
+    public class MainDbContext : DbContext {
+        public MainDbContext() : base("name=DefaultConnection") {
+        }
+        public DbSet<DataModel> Users { get; set; }
+
+    }
+}
---
+++ ScrapeGmail/Models/DataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScrapeGmail.Models {
+    public class DataModel {
+        [Key]
+        public int Id { get; set; }
+        public string Details { get; set; }
+
+        public string Date_Posted { get; set; }
+        public string Time_Posted { get; set; }
+        public string Date_Edited { get; set; }
+        public string Time_Edited { get; set; }
+        public string Public { get; set; }
+        public int User_Id { get; set; }
+    }
+}
